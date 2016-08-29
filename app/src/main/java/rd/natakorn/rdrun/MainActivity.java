ead97b3db4bddab54ce80aab684053ef583a89b8
onibaku <scnataktam@gmail.com>
2016-08-29 14:29:16 +0700

    Change Activity to SignUp

--- app/src/main/java/rd/natakorn/rdrun/MainActivity.java
+++ app/src/main/java/rd/natakorn/rdrun/MainActivity.java
@@ -1,7 +1,9 @@
 package rd.natakorn.rdrun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }//  Maim Method
+    // Get Event from Click Button
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this,SignUpActivity.class));
     }
-}
+
+    public void clickSignIn(View view) {
+
+    }
+
+
+}//  Main Class
