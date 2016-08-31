72b751718321c34d8dc3983518a48ac7c40d0024
onibaku <scnataktam@gmail.com>
2016-08-31 15:56:48 +0700

    Athen Success

--- app/src/main/java/rd/natakorn/rdrun/MainActivity.java
+++ app/src/main/java/rd/natakorn/rdrun/MainActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -18,6 +19,9 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,8 +56,11 @@ public class MainActivity extends AppCompatActivity {
 
         //  Explicit
         private Context context;
-        private String myUserString, mypasswordString;
+        private String myUserString, mypasswordString , truePasswordString,
+                        nameString ,surnameString ,idString;
         private static final String urlJSON="http://swiftcodingthai.com/rd/get_user_master.php";
+        private boolean statusABoolean = true;
+
 
         public SynUser(Context context, String myUserString, String mypasswordString) {
             this.context = context;
@@ -85,6 +92,47 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(s);
 
             Log.d("31AugV2", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                for (int i=0;i<jsonArray.length();i+=1) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    if (myUserString.equals(jsonObject.getString("User"))) {
+
+                        statusABoolean = false;
+                        truePasswordString = jsonObject.getString("Password");
+                        nameString = jsonObject.getString("Name");
+                        surnameString = jsonObject.getString("Surname");
+                        idString = jsonObject.getString("id");
+                    }  // Ifse
+
+                }  // For
+
+                if (statusABoolean) {
+                    // User Fail
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, R.drawable.nobita48, "User False",
+                            "ไม่มี " + myUserString + " อยู่ในฐานข้อมูล");
+                } else if (mypasswordString.equals(truePasswordString)) {
+                    // Password True
+                    Toast.makeText(context,"Wellcome "+ nameString+" "+surnameString,Toast.LENGTH_SHORT).show();
+
+                } else {
+                    // Password false
+
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context,R.drawable.doremon48,"Password Flase", "Please Try Again");
+
+                }
+
+
+            } catch (Exception e) {
+                Log.d("31AugV2", "e onPost ==>" + e.toString());
+
+            }
 
         }  // On Post
 
